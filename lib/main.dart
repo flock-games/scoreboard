179d3dcf432fcebfb3a72a7b6fda942d02c445db
Tyler Dawson <tddawson@gmail.com>
2025-10-06 17:01:55 -0700

    Adds first settings: reseting scores and configuring number of players

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:starter/ScorePanel.dart';
+import 'package:starter/SettingsPanel.dart';
+import 'package:starter/models/Player.dart';
 
 void main() {
   runApp(const ScoreboardApp());
@@ -31,33 +33,101 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Player> players = [
+    Player(
+        name: 'Player 1',
+        score: 0,
+        bgColor: Colors.primaries[0],
+        textColor: Colors.white),
+  ];
+  int numPlayers = 2;
+  bool showingSettings = false;
+
+  void resetScores() {
+    setState(() {
+      for (var player in players) {
+        player.score = 0;
+      }
+    });
+  }
+
+  void setNumPlayers(int n) {
+    setState(() {
+      players.clear();
+      for (int i = 0; i < n; i++) {
+        players.add(Player(
+          name: 'Player ${i + 1}',
+          score: 0,
+          bgColor: Colors.primaries[i % Colors.primaries.length],
+          textColor: Colors.white,
+        ));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: OrientationBuilder(
+    return Material(
+      child: OrientationBuilder(
           builder: (BuildContext context, Orientation orientation) {
-        const scorePanels = [
-          Expanded(
-            child: ScorePanel(
-              panelColor: Colors.blue,
-              textColor: Colors.white,
+        List<Widget> scorePanels = <Widget>[];
+
+        for (Player player in players) {
+          scorePanels.add(
+            Expanded(
+              child: ScorePanel(
+                score: player.score,
+                panelColor: player.bgColor,
+                textColor: player.textColor,
+                onIncrementScore: () => {
+                  setState(() {
+                    player.score++;
+                  })
+                },
+              ),
             ),
-          ),
-          Expanded(
-            child: ScorePanel(
-              panelColor: Colors.red,
-              textColor: Colors.white,
-            ),
-          ),
-        ];
-        if (orientation == Orientation.landscape) {
-          return const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: scorePanels,
           );
         }
-        return const Column(
+
+        if (orientation == Orientation.landscape) {
+          return Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: scorePanels,
+              ),
+              IconButton(
+                icon: const Icon(Icons.settings, color: Colors.white),
+                onPressed: () {
+                  setState(() {
+                    showingSettings = !showingSettings;
+                  });
+                },
+                padding: const EdgeInsets.all(16),
+                iconSize: 32,
+                style: IconButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  shape: const CircleBorder(),
+                ),
+              ),
+              if (showingSettings)
+                Positioned(
+                  bottom: 60,
+                  child: Container(
+                    color: Colors.black,
+                    child: SettingsPanel(
+                      numPlayers: players.length,
+                      onResetScores: resetScores,
+                      onSetNumPlayers: setNumPlayers,
+                    ),
+                  ),
+                ),
+            ],
+          );
+        }
+        return Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: scorePanels,
